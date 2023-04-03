1031925d16537575673b0419276f5310b2a5fc9f
DevFabrizio <rockout94@hotmail.it>
2023-04-03 12:34:27 +0000

    define function to substitute the normal print statement for slow typing effect

--- run.py
+++ run.py
@@ -1,5 +1,12 @@
 import time,sys
 
+"""
+Function to substitute the normal print function in order to create a slow typing effect
+"""
 
-
-
+def typingPrint(text):
+  for character in text:
+    sys.stdout.write(character)
+    sys.stdout.flush()
+    time.sleep(0.02)
+  
