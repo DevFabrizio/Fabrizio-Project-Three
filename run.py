fe19cb2c7046f2ca413790b1c727dfcaef941fc7
DevFabrizio <rockout94@hotmail.it>
2023-04-03 14:13:49 +0000

    add function to substitute the input function with a slow typing effect

--- run.py
+++ run.py
@@ -1,12 +1,31 @@
-import time,sys
+import time, sys
 
-"""
-Function to substitute the normal print function in order to create a slow typing effect
-"""
 
-def typingPrint(text):
-  for character in text:
-    sys.stdout.write(character)
-    sys.stdout.flush()
-    time.sleep(0.02)
+def typing_print(text):
+
+    """
+    Function to substitute the normal print function to create a slow typing effect
+    """
+
+    for character in text:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(0.02)
   
+  
+def typing_input(text):
+   
+    """
+    Function to substitute the normal imput function with a slow typing effect 
+    """
+  
+    for character in text:
+        sys.stdout.write(character)
+        sys.stdout.flush()
+        time.sleep(0.05)
+    value = input()
+    return value
+
+
+
+
