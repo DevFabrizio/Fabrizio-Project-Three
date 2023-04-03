d96c6471a7898bd599a75995b239895156f753bd
DevFabrizio <rockout94@hotmail.it>
2023-04-03 14:16:39 +0000

    add introduction and rules

--- run.py
+++ run.py
@@ -26,6 +26,8 @@ def typing_input(text):
     value = input()
     return value
 
+typing_print("Welcome to this round of BattleShip!\n In case you have never played this game I will now explain the rules.\n The game will generate a grid and both you and the computer\n will be assigned some ships on this grid.\n You will only be able to see your ships' positions and when prompted you'll have to decide where to fire on the enemy's grid.\n Once you have sunk all the computer's ships you will have won\n but if the enemy sinks all of your ships you will have lost.\n Now get ready and have fun!\n")
+print()
 
 
 
