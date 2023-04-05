65c72789f011ce9a9108c8571545dcaae2ad278c
DevFabrizio <rockout94@hotmail.it>
2023-04-05 06:36:37 +0000

    add validation for username

--- run.py
+++ run.py
@@ -3,35 +3,44 @@ import sys
 import data
 
 
-def typing_print(text):
+def typing_print(text, t):
 
     """
-    Function to substitute the normal print function to create slow typing effect
+    Function to substitute the print function to create slow typing effect
     """
 
     for character in text:
         sys.stdout.write(character)
         sys.stdout.flush()
-        time.sleep(0.02)
-  
-  
-def typing_input(text):
-   
-    """
-    Function to substitute the normal imput function with a slow typing effect 
-    """
- 
-    for character in text:
-        sys.stdout.write(character)
-        sys.stdout.flush()
-        time.sleep(0.05)
-    value = input()
-    return value
+        time.sleep(t)
+    return ''
 
 
-typing_print(data.RULES)
+# def typing_input(text, t):
 
-print()
+#     """
+#     Function to substitute the normal imput function with a slow 
+#     typing effect
+#     """
+
+#     for character in text:
+#         sys.stdout.write(character)
+#         sys.stdout.flush()
+#         time.sleep(t)
+#     value = input()
+#     return value
 
 
+typing_print(data.RULES, 0.02)
+
+USER = ''
+
+while True:
+    USER = input(typing_print('Enter your battle name: ', 0.02))
+
+    if USER.isalpha():
+        typing_print(f"Welcome {USER}, get ready for battle!", 0.02)
+        break
+    else:
+        typing_print('You must enter a valid name!\n', 0.02)
 
